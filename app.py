6d7b9710e3a77d4a3da9fc48379e6726f5fbc662
Francisco Zanartu <fzanartu@outlook.com>
2024-09-04 14:27:33 -0400

    integrate debunker module into pipeline

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 """Fact-checking pipeline and Gradio interface for claim verification."""
 
 import logging
-from src.search import keyword_search_result, get_urls
-from src.retrieve import (
+import json
+from src.fact_check.search import keyword_search_result, get_urls
+from src.fact_check.retrieve import (
     fetch_url_and_parse_html,
     split_into_chunked_docs,
     retrieve_docs,
 )
-from src.process import stance_detection, merge_answer
-from src.aux import clean_and_match
+from src.fact_check.process import stance_detection, merge_answer
+from src.fact_check.aux import clean_and_match
+from src.debunker.core import Debunker
 import gradio as gr
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
@@ -31,7 +33,9 @@ def fact_checking_pipeline(claim):
 
     # 3. Stance Detection:
     labels = stance_detection(model, evidence_docs, claim)
+    logging.info("labels = %s", json.dumps(labels, indent=4))
     lbls = clean_and_match(labels)
+    logging.info("lbls = %s", json.dumps(lbls, indent=4))
 
     # 4. Claim Validation:
     answer = merge_answer(model, evidence_docs, lbls, claim)
@@ -42,22 +46,27 @@ def fact_checking_pipeline(claim):
         except AttributeError:
             content = answer[1]
 
-        return str(
-            {
-                "answer": content,
-                "keywords": keywords,
-                "source": answer[2],
-                "stance": answer[0],
-            }
-        )
-    return str(
-        {
+        factual_response = {
+            "answer": content,
+            "keywords": keywords,
+            "source": answer[2],
+            "stance": answer[0],
+        }
+
+    else:
+        factual_response = {
             "answer": answer,
             "keywords": keywords,
             "source": "NA",
             "stance": "Unknown",
         }
-    )
+
+    debunker = Debunker(model)
+    rebuttal = debunker.rebuttal_generator(claim, factual_response)
+
+    factual_response["rebuttal"] = rebuttal
+
+    return str(factual_response)
 
 
 demo = gr.Interface(fn=fact_checking_pipeline, inputs="text", outputs="text")
